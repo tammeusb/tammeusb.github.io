96656a940571b9cdd698509b0412f0293c37c513
Brock <tammeusb@gmail.com>
2024-06-01 16:01:14 -0400

    Add script to play audio on first clip of 3

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ const clip = document.querySelector('.track-3 .clip.two');
 
 clip.addEventListener('click', () => {
     console.log('hello');
+    playAudio();
 });
+
+function playAudio () {
+    const audio = new Audio('press.wav');
+    audio.play();
+}
