28c8cc59267070966fb5574e7c7803d5b6e71bfa
Brock <tammeusb@gmail.com>
2024-06-02 22:08:25 -0400

    Add basic movePlayHead() function and button

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ function playAudio (id) {
     const audio = new Audio(id);
     audio.play();
 }
+
+const playButton = document.querySelector('button.play');
+const playHead = document.querySelector('.playhead');
+
+playButton.addEventListener('click', () => {
+    console.log('click');
+    movePlayhead();
+});
+
+function movePlayhead() {
+    let count = 0;
+    let leftPosition = 30;
+
+    const timer = setInterval(function() {
+        playHead.style.left = leftPosition + 'vh';
+        leftPosition++;
+        console.log(leftPosition);
+        if (leftPosition > 80) clearInterval(timer);
+    },200);
+
+}
